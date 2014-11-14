1b052b431bcd2b0d8fa5b35e368f04198d5b1734
cdean15 <cdean15@uga.edu>
2014-11-13 20:08:20 -0500

    testing push, MembershipManager in progress

--- Dawg Trade/src/edu/uga/dawgtrades/persist/impl/MembershipManager.java
+++ Dawg Trade/src/edu/uga/dawgtrades/persist/impl/MembershipManager.java
@@ -1,5 +1,27 @@
 package edu.uga.dawgtrades.persist.impl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import edu.uga.dawgtrades.model.Membership;
+import edu.uga.dawgtrades.model.ObjectModel;
+
+
 public class MembershipManager {
 
+	private ObjectModel objectModel = null;
+	private Connection conn = null;
+	
+	public MembershipManager(Connection conn, ObjectModel objectModel){
+		this.conn = conn;
+		this.objectModel = objectModel;
+	}
+	
+	
+	
 }
